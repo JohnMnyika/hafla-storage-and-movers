3436b50c01556ef977f588a843c1a067a8903915
JohnMnyika <jmnyika514@gmail.com>
2025-02-14 07:43:22 +0300

    Under construction

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,8 @@
+import '../styles/globals.css'; // Adjust the path as needed
 import type { AppProps } from 'next/app';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '../styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <>
-            <Component {...pageProps} />
-            <ToastContainer />
-        </>
-    );
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />;
 }
+
+export default MyApp;
